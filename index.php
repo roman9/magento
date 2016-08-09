f26d6de95dc07cff2d93be92415ef9aa3652167c
Rts Rts <rts@gomage.com>
2016-08-09 18:14:37 +0300

    Merge remote-tracking branch 'origin/master' into myfirst
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -1,37 +1,3 @@
 <?php
 echo 'test 12344';
 
-
-echo 'test 12344';
-echo 'test 12344';
-echo 'test 12344';
-
-
-echo 'test 12w344';
-echo 'test 1234w4';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-echo 'test 12w44';
-
-
-echo 'test 12344';
-echo 'test 12344';
-echo 'test 12344';
